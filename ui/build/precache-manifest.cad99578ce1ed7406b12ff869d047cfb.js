4479163ab40f50192dfaaee6db22fc86ff1ee23e
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-03-14 23:43:38 -0500

    Test commit

---
+++ ui/build/precache-manifest.cad99578ce1ed7406b12ff869d047cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fdf4bd7f6e2aed8cad879358a5a9a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd2f1c0ea2e736cd3bf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5bdc73834871d48f5f2a",
+    "url": "/static/js/2.002e928c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.002e928c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd2f1c0ea2e736cd3bf",
+    "url": "/static/js/main.be93eedd.chunk.js"
+  },
+  {
+    "revision": "9e19e0944bfc6625df6b",
+    "url": "/static/js/runtime-main.e0c44f74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
